f1b62965e1aa14ec9a88633a24157c4609f96a50
nhatktvn2001 <nhatktvn2001@gmail.com>
2023-02-06 23:18:52 +0700

    first deploy

--- backend/server.js
+++ backend/server.js
@@ -43,12 +43,16 @@ app.use('/api/users', userRouter);
 app.use('/api/send', sendEmailRouter);
 
 app.use('/api/upload', uploadRouter);
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+// const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+// app.get('*', (req, res) =>
+//     res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+// );
 
-app.listen(5000, () => {
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
     console.log("hello at http://localhost:5000");
 });
