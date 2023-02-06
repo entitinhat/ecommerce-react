fb4c111ab8aa1a7b2bd92da65958e20ac6bea36d
nhatktvn2001 <nhatktvn2001@gmail.com>
2023-02-06 19:03:27 +0700

    add mongodb_url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost/base-react-redux', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 
