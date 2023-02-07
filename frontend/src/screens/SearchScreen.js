576b5a20401c2d318bc522df3dad7eeba9eeb02c
nhatktvn2001 <nhatktvn2001@gmail.com>
2023-02-07 13:07:20 +0700

    search screen modified deploy backend api

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -97,7 +97,7 @@ export default function SearchScreen(props) {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const { data } = await axios.get(`/api/products/search?query=${query}&type=${type}&category=${category}&price=${price}&color=${color}&order=${order}`);  // 13.1 Part 2
+                const { data } = await axios.get(`https://fourmens-ecommerce-website.onrender.com/api/products/search?query=${query}&type=${type}&category=${category}&price=${price}&color=${color}&order=${order}`);  // 13.1 Part 2
                 dispatch({
                     type: 'FETCH_SUCCESS',
                     payload: data
@@ -118,7 +118,7 @@ export default function SearchScreen(props) {
     useEffect(() => {
         const fetchCategories = async () => {
             try {
-                const { data } = await axios.get('/api/products/categories');
+                const { data } = await axios.get('https://fourmens-ecommerce-website.onrender.com/api/products/categories');
                 setCategories(data);  // ["Pants","Shirts"]
             }
             catch (error) {
@@ -135,7 +135,7 @@ export default function SearchScreen(props) {
         const filterPrice = filter.price || price;
         const filterColor = filter.color || color;
         const sortOrder = filter.order || order  // 13.1 Part 3
-        return `/search?query=${filterQuery}&type=${filterType}&category=${filterCategory}&price=${filterPrice}&color=${filterColor}&order=${sortOrder}`// 13.1 Part 4
+        return `https://fourmens-ecommerce-website.onrender.com/search?query=${filterQuery}&type=${filterType}&category=${filterCategory}&price=${filterPrice}&color=${filterColor}&order=${sortOrder}`// 13.1 Part 4
     }
 
     // const hideShowFilter = () => {
